f616609f6b7d3d7f87667e88e2fc52c29b09339f
seal0112 <seal0112@gmail.com>
2020-05-31 03:29:32 +0800

    [fix] fix data order problem in front-end restful api

--- app/frontend/frontendModel.py
+++ app/frontend/frontendModel.py
@@ -22,6 +22,20 @@ from sqlalchemy.sql import func
 # console.setFormatter(formatter)
 # logger.addHandler(console)
 
+@frontend.route('/daily_info/<stock_id>')
+def getFrontEndDailyInfo(stock_id):
+    dailyInfo = db.session\
+        .query()\
+        .with_entities(
+            Daily_Information.本日收盤價,
+            Daily_Information.本日漲跌,
+            Daily_Information.本益比,
+            Daily_Information.近四季每股盈餘)\
+        .filter_by(stock_id=stock_id)\
+        .one_or_none()
+    data = dailyInfo._asdict()
+    return jsonify(data)
+
 
 @frontend.route('/month_revenue/<stock_id>')
 def getFrontEndMonthRevenue(stock_id):
@@ -29,8 +43,8 @@ def getFrontEndMonthRevenue(stock_id):
         .query()\
         .with_entities(
             func.concat(
-                Income_Sheet.year, 'Q', Income_Sheet.season).label(
-                    "Year/Season"),
+                Month_Revenue.year, '/', Month_Revenue.month).label(
+                    "Year/Month"),
             Month_Revenue.當月營收,
             Month_Revenue.去年同月增減)\
         .filter_by(stock_id=stock_id)\
@@ -47,28 +61,14 @@ def getFrontEndEPS(stock_id):
         .query()\
         .with_entities(
             func.concat(
-                Income_Sheet.year, 'Q', Income_Sheet.season).label(
+                Income_Sheet.year, '-', Income_Sheet.season).label(
                     "Year/Season"),
             Income_Sheet.基本每股盈餘)\
         .filter_by(stock_id=stock_id)\
-        .order_by("Year/Season")\
+        .order_by(Income_Sheet.year.desc())\
+        .order_by(Income_Sheet.season.desc())\
         .limit(20).all()
     data = [row._asdict() for row in EPS]
-    return jsonify(data)
-
-
-@frontend.route('/daily_info/<stock_id>')
-def getFrontEndDailyInfo(stock_id):
-    dailyInfo = db.session\
-        .query()\
-        .with_entities(
-            Daily_Information.本日收盤價,
-            Daily_Information.本日漲跌,
-            Daily_Information.本益比,
-            Daily_Information.近四季每股盈餘)\
-        .filter_by(stock_id=stock_id)\
-        .one_or_none()
-    data = dailyInfo._asdict()
     return jsonify(data)
 
 
@@ -87,7 +87,8 @@ def getFrontEndIncomeSheet(stock_id):
             Income_Sheet.本期淨利,
             Income_Sheet.母公司業主淨利)\
         .filter_by(stock_id=stock_id)\
-        .order_by("Year/Season")\
+        .order_by(Income_Sheet.year.desc())\
+        .order_by(Income_Sheet.season.desc())\
         .limit(20).all()
     data = [row._asdict() for row in incomeSheet]
     return jsonify(data)
@@ -106,7 +107,8 @@ def getFrontEndProfitAnalysis(stock_id):
             Income_Sheet.稅前淨利率,
             Income_Sheet.本期淨利率)\
         .filter_by(stock_id=stock_id)\
-        .order_by("Year/Season")\
+        .order_by(Income_Sheet.year.desc())\
+        .order_by(Income_Sheet.season.desc())\
         .limit(20).all()
     data = [row._asdict() for row in profit]
     return jsonify(data)
@@ -129,7 +131,8 @@ def getFrontEndOperationExpenseAnalysis(stock_id):
             Income_Sheet.管理費用,
             Income_Sheet.研究發展費用)\
         .filter_by(stock_id=stock_id)\
-        .order_by("Year/Season")\
+        .order_by(Income_Sheet.year.desc())\
+        .order_by(Income_Sheet.season.desc())\
         .limit(20).all()
     data = [row._asdict() for row in operationExpense]
     return jsonify(data)
