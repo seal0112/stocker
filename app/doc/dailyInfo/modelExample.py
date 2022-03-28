bf6eb0189db3616c793bf52252ba1089e340643e
ckyuo <cykuotw45@gmail.com>
2022-03-27 23:29:28 -0400

    [doc] Add daily info api document

---
+++ app/doc/dailyInfo/modelExample.py
@@ -0,0 +1,8 @@
+dailyInfoResponseExample = {
+  "stock_id": "2330", 
+  "update_date": "Fri, 25 Mar 2022 00:00:00 GMT", 
+  "本日收盤價": 598.0, 
+  "本日漲跌": 7.0, 
+  "本益比": 25.99, 
+  "近四季每股盈餘": 23.01
+}
