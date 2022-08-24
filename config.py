929d9bda6887de84c05a683e235be592d8359962
cykuo <cykuotw45@gmail.com>
2022-08-23 23:10:22 -0400

    [fix] database account

--- config.py
+++ config.py
@@ -7,11 +7,19 @@ with open('{}/critical_file/client_secret.json'.format(
         basedir)) as clientSecretReader:
     client_secret = json.loads(clientSecretReader.read())
 
-DB_URL = """mysql+pymysql://%s:%s@%s/stocker?charset=utf8""" % (
-    os.getenv('DB_USER'),
-    os.getenv('DB_PASSWORD'),
-    os.getenv('DB_HOST'))
+with open('{}/critical_file/databaseAccount.json'.format(
+        basedir)) as databaseAccountReader:
+    database_account = json.loads(databaseAccountReader.read())
 
+# DB_URL = """mysql+pymysql://%s:%s@%s/stocker?charset=utf8""" % (
+#     os.getenv('DB_USER'),
+#     os.getenv('DB_PASSWORD'),
+#     os.getenv('DB_HOST'))
+
+DB_URL = """mysql+pymysql://%s:%s@%s/stocker?charset=utf8""" % (
+    database_account['username'],
+    database_account['password'],
+    database_account['ip'])
 
 class Config:
     SECRET_KEY = os.environ.get('SECRET_KEY') or 'hard to guess db.String'
