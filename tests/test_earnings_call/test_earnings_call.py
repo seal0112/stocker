973a33100a75595b3c8e72326ee4969c05a9fecf
Raymond Hu <seal0112@gmail.com>
2024-01-20 11:43:44 +0800

    feat: add otc test stock

--- tests/test_earnings_call/test_earnings_call.py
+++ tests/test_earnings_call/test_earnings_call.py
@@ -14,7 +14,7 @@ class TestEarningsCallService():
     def test_create_earnings_call(self):
         assert 1 == 1
 
-    @pytest.mark.parametrize("stock_id", ["2330", "2454"])
+    @pytest.mark.parametrize("stock_id", ["2330", "2454", "5474"])
     @pytest.mark.parametrize("meeting_date", [date.today().strftime('%Y-%m-%d')])
     def test_get_stock_all_earnings_call(self, stock_id, meeting_date):
         earnings_calls = earnings_call_service.get_stock_all_earnings_call(stock_id, meeting_date)
